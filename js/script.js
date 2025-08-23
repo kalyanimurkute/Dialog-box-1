69da0df970de0ac3ca6b2258afb954528273c174
kalyanimurkute <murkutekalyani259@gmail.com>
2025-08-23 19:11:24 +0530

    Create script.js
    add script file

---
+++ js/script.js
@@ -0,0 +1,7 @@
+ function openModal() {
+      document.getElementById('overlay').style.display = 'flex';
+    }
+
+    function closeModal() {
+      document.getElementById('overlay').style.display = 'none';
+    }
